a68e656aa613d958222f19a53b9e1cae2dd1bb5a
Karen <karenazoulay92@gmail.com>
2025-08-04 19:12:43 +0200

    ⚛ Feat: add tagAnswer for use in CardInformative

---
+++ components/atoms/TagAnswer.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  isSelected: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+const {isSelected} = props
+
+const onClick = () => {
+console.log('click')
+}
+
+const classNames = computed(() => {
+  return ['tag-answer', {'is-selected' : isSelected}]
+})
+</script>
+
+<template>
+<li :class="classNames">
+  <button @click="onClick">
+    {{name}}
+  </button>
+
+</li>
+</template>
+
+<style scoped>
+
+</style>
