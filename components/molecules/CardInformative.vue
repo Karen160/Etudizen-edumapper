220727f82d7f0941e651f04d02f604dd7f1655b8
Karen <karenazoulay92@gmail.com>
2025-08-04 11:17:46 +0200

    🧬 Feat : add & config props for molecule CardInformative

---
+++ components/molecules/CardInformative.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  isOpen: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answers: {
+    type: Array,
+    required: false,
+    default: () => []
+  },
+  status: {
+    type: String,
+    required: false,
+    default: null
+  }
+})
+</script>
+
+<template>
+<div class="card-informative">
+  <p>{{name}}</p>
+</div>
+</template>
--- components/templates/TplForm.vue
+++ components/templates/TplForm.vue
@@ -1,5 +1,7 @@
 <template>
-
+<div>
+  <CardInformative name="CardInformative" />
+</div>
 </template>
 
 <style scoped>
