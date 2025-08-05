003a5df96585dfd990dc96c91249f8ebca7811cc
Karen <karenazoulay92@gmail.com>
2025-08-05 19:09:46 +0200

    🗑️ Fix: delete folder .netlify

--- .netlify/functions-internal/server/chunks/build/BaseHeader-styles.CDE3QmLM.mjs
+++
@@ -1,6 +0,0 @@
-import { B as BaseHeader_vue_vue_type_style_index_0_scoped_a5d0e3e0_lang } from './BaseHeader-styles-1.mjs-CgGfJ9M_.mjs';
-
-const BaseHeaderStyles_CDE3QmLM = [BaseHeader_vue_vue_type_style_index_0_scoped_a5d0e3e0_lang];
-
-export { BaseHeaderStyles_CDE3QmLM as default };
-//# sourceMappingURL=BaseHeader-styles.CDE3QmLM.mjs.map
--- .netlify/functions-internal/server/chunks/build/BaseHeader-styles.DILJwPhX.mjs
+++
@@ -1,6 +0,0 @@
-import { B as BaseHeader_vue_vue_type_style_index_0_scoped_a5d0e3e0_lang } from './BaseHeader-styles-1.mjs-CgGfJ9M_.mjs';
-
-const BaseHeaderStyles_DILJwPhX = [BaseHeader_vue_vue_type_style_index_0_scoped_a5d0e3e0_lang, BaseHeader_vue_vue_type_style_index_0_scoped_a5d0e3e0_lang];
-
-export { BaseHeaderStyles_DILJwPhX as default };
-//# sourceMappingURL=BaseHeader-styles.DILJwPhX.mjs.map
