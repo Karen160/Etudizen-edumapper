76c9dcf262e95ca7ddea8bc05aa2be1985d77089
Karen <karenazoulay92@gmail.com>
2025-08-05 18:45:45 +0200

    🚀 Fix: fix config deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
+    ssr: false,
+    nitro: {
+        preset: 'netlify'
+    },
     css: ['~/assets/css/main.css'],
     vite: {
         plugins: [
